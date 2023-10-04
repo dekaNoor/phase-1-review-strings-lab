a7b70b07022ed98c853c432f6850ba801b8d9c63
dekaNoor <deka.ali@student.moringaschool.com>
2023-10-04 12:50:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'DNA' ;
+const welcomeMessage = 'Welcome to Flatbook, DNA!' ;
+
+const excitedWelcomeMessage ='WELCOME TO FLATBOOK, DNA!';
+const shortGreeting = 'Welcome, D!';
+
